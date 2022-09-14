8d0912d849058d180a7672eceb05faf837afc564
Albert2049 <makacio55@gmail.com>
2022-09-13 23:26:59 -0500

    Users login is add and working fine

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { validatorRegister, validatorLogin } = require('../validators/authValidator');
-const { registerCtrl } = require('../controllers/authController');
+const { registerCtrl, loginCtrl } = require('../controllers/authController');
 
 
 router.post('/register', validatorRegister, registerCtrl);
+
+router.post('/login', validatorLogin, loginCtrl);
 
 
 module.exports = router;
